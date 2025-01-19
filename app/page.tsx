0a17af1a2007adc469a92e833b711f88de890b82
mirzaalamin <mjapurbo2@gmail.com>
2025-01-19 18:53:59 +0600

    Assets and fonts setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+/* eslint-disable react/react-in-jsx-scope */
 export default function Home() {
   return (
     <>
-      <h2 className="text-3xl font-bold">Welcome to the NEXT js world</h2>
+      <h2 className="text-3xl font-inter font-bold">
+        Welcome to the NEXT js world
+      </h2>
+      <h2 className="text-3xl font-space-grotesk font-bold">
+        Welcome to the NEXT js world
+      </h2>
     </>
   );
 }
