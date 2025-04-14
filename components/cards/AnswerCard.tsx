78caea746ec29031c56a8cdfe26f290fd330da6b
mirzaalamin <mjapurbo2@gmail.com>
2025-04-14 15:14:51 +0600

    Implement Answer Votes

--- components/cards/AnswerCard.tsx
+++ components/cards/AnswerCard.tsx
@@ -1,11 +1,25 @@
 import ROUTES from "@/constants/routes";
+import { hasVoted } from "@/lib/actions/vote.action";
 import { getTimeStamp } from "@/lib/utils";
 import Link from "next/link";
+import { Suspense } from "react";
 import UserAvatar from "../UserAvatar";
 import Preview from "../editor/Preview";
+import Votes from "../votes/Votes";
 
 /* eslint-disable react/react-in-jsx-scope */
-const AnswerCard = ({ _id, author, content, createdAt }: Answer) => {
+const AnswerCard = ({
+  _id,
+  author,
+  content,
+  createdAt,
+  upvotes,
+  downvotes,
+}: Answer) => {
+  const hasVotedPromise = hasVoted({
+    targetId: _id,
+    targetType: "answer",
+  });
   return (
     <article className="light-border border-b py-10">
       <span id={JSON.stringify(_id)} className="hash-span" />
@@ -32,7 +46,17 @@ const AnswerCard = ({ _id, author, content, createdAt }: Answer) => {
             </p>
           </Link>
         </div>
-        <div className="flex justify-end">Votes</div>
+        <div className="flex justify-end">
+          <Suspense>
+            <Votes
+              upvotes={upvotes}
+              downvotes={downvotes}
+              targetType="answer"
+              targetId={_id}
+              hasVotedPromise={hasVotedPromise}
+            />
+          </Suspense>
+        </div>
       </div>
       <Preview content={content} />
     </article>
