87f9e156fae8e5c84ee7e0dc1f62bbe4e1660919
mirzaalamin <mjapurbo2@gmail.com>
2025-04-21 12:51:44 +0600

    Develop Collections Page

--- app/(root)/collections/page.tsx
+++ app/(root)/collections/page.tsx
@@ -1,6 +1,59 @@
 /* eslint-disable react/react-in-jsx-scope */
-const Collections = () => {
-  return <div>Collections</div>;
+
+import QuestionCard from "@/components/cards/QuestionCard";
+import DataRenderer from "@/components/DataRenderer";
+import LocalSearch from "@/components/search/LocalSearch";
+import { EMPTY_COLLECTIONS } from "@/constants/states";
+
+import ROUTES from "@/constants/routes";
+import { getSavedQuestions } from "@/lib/actions/collection.action";
+import "../../../database/index";
+
+interface SearchParams {
+  searchParams: Promise<{ [key: string]: string }>;
+}
+
+const Collections = async ({ searchParams }: SearchParams) => {
+  // const session = await auth();
+
+  const { page, pageSize, query, filter } = await searchParams;
+
+  const { success, data, error } = await getSavedQuestions({
+    page: Number(page) || 1,
+    pageSize: Number(pageSize) || 10,
+    query: query || "",
+    filter: filter || "",
+  });
+
+  const { collection } = data || {};
+
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900">Saved Questions</h1>
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearch
+          route={ROUTES.COLLECTIONS}
+          imgSrc="/icons/search.svg"
+          placeholder="Search for a Question here..."
+          otherClasses=""
+        />
+      </div>
+
+      <DataRenderer
+        success={success}
+        error={error}
+        data={collection}
+        empty={EMPTY_COLLECTIONS}
+        render={(collection) => (
+          <section className="mt-10 flex w-full flex-col gap-6">
+            {collection.map((item) => (
+              <QuestionCard key={item._id} question={item.question} />
+            ))}
+          </section>
+        )}
+      />
+    </>
+  );
 };
 
 export default Collections;
--- constants/states.ts
+++ constants/states.ts
@@ -52,6 +52,6 @@ export const EMPTY_COLLECTIONS = {
     "Looks like you haven’t created any collections yet. Start curating something extraordinary today",
   button: {
     text: "Save to Collection",
-    href: ROUTES.COLLECTION,
+    href: ROUTES.HOME,
   },
 };
